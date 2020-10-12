2f444258354fbad80d0b3cfd909a5a4db53ae437
Zeng Tao <qingchun1995@gmail.com>
2020-10-12 21:17:46 +0800

    AddEducation, AddExperience, EditProfile pages

---
+++ client/src/views/Profile/AddEducation.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AddEducation: React.FC = () => {
+  return <div>Add Education</div>
+}
+
+export { AddEducation }
---
+++ client/src/views/Profile/EditProfile.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const EditProfile: React.FC = () => {
+  return <div>Edit Profile</div>
+}
+
+export { EditProfile }
